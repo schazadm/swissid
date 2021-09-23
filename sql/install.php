824c7affda93351c119a12a3f8464e01900a2d04
Schazad M <schazad.mohammad@hotmail.com>
2021-09-23 17:23:59 +0200

    customer id is now unique in the table

--- sql/install.php
+++ sql/install.php
@@ -27,10 +27,9 @@ $sql = [];
 
 $sql[] = 'CREATE TABLE IF NOT EXISTS `' . _DB_PREFIX_ . 'swissid_customer` (
     `id_swissid_customer` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-    `id_customer` INT(10) UNSIGNED NOT NULL UNIQUE,
+    `id_customer` INT(10) UNSIGNED UNIQUE NOT NULL,
     `age_over` TINYINT(1) UNSIGNED DEFAULT 0,
-    PRIMARY KEY  (`id_swissid_customer`),
-    KEY `id_customer` (`id_customer`)
+    PRIMARY KEY  (`id_swissid_customer`)
 ) ENGINE=' . _MYSQL_ENGINE_ . ' DEFAULT CHARSET=utf8;';
 
 foreach ($sql as $query) {
